2736739a3151fe803bc5fc1261c604aa883b1741
tutoriagit <gabriellamazon08@gmail.com>
2024-02-03 15:34:12 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'mochawesome',
+  "reporterOptions": {
+    "reportDir": "mochawesome-report",
+    "overwrite": false,
+    "html": true,
+    "json": true
+  },
   e2e: {
     specPattern: "cypress/e2e/step_definitions/*.feature",
     setupNodeEvents(on, config) {
